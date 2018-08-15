ebc991f177c50049803a26dc5fd9961b26a3bee6
Tomohide Takao <amakunai.dorayaki@gmail.com>
2018-08-15 21:17:41 +0900

    feat: Make enable to response a HTML file

--- actix_web/src/main.rs
+++ actix_web/src/main.rs
@@ -1,13 +1,16 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+use actix_web::{http::Method, server, App, HttpRequest, Responder};
 
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello, World!"
+fn index(_req: &HttpRequest) -> impl Responder {
+    "Hello, Actix Web!"
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("localhost:3030")
-        .unwrap()
-        .run();
+    server::new(|| {
+        App::new()
+            .prefix("/app")
+            .resource("/index.html", |r| r.method(Method::GET).f(index))
+    }).bind("localhost:3030")
+    .unwrap()
+    .run();
 }
